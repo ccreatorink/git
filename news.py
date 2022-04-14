43ba10daa9939bc769bce7cb975ab8d4620f7ee6
ccreatorink <98666451+ccreatorink@users.noreply.github.com>
2022-04-14 16:25:28 -0400

    Add files via upload

---
+++ news.py
@@ -0,0 +1,9 @@
+x = "/.2/"
+
+def myfunc():
+  x = "/1,V5/V5 is here! can you belive it?were now in alpha testing stage at 0.5! very soon,Creator'sInk will be switching to python,but we'll see how that goes."
+  print("Update 1" + x)
+
+myfunc()
+
+print("Update 2" + x)
